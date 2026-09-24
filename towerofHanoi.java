dab60e70a647ac7ecff65387d1aabc2c7fa94a46
Rehana <rehanalamichhane6@gmail.com>
2026-09-24 14:09:08 +0545

    Add files via upload

---
+++ towerofHanoi.java
@@ -0,0 +1,16 @@
+class towerofHanoi{
+    public static void tower(int n,String src,String helper,String dest){
+       if(n==1){
+        System.out.println("transfer disk"+n+"from"+src+"to"+dest);
+        return;
+       }
+       
+        tower(n-1,src,dest,helper);
+        System.out.println("transfer disk"+n+"from"+src+"to"+dest);
+        tower(n-1,helper,src,dest);
+    }
+    public static void main(String[]main){
+        int n=3;
+        tower(n,"A","B","C");
+    }
+}
